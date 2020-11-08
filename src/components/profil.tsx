e29070d0fab76219a899f48d8d836377fc121e7a
Chiquet Daniel <daniel.chiquet@ynov.com>
2020-11-08 22:20:13 +0100

    ajout d'une alert pour le changement de username

--- src/components/profil.tsx
+++ src/components/profil.tsx
@@ -1,25 +1,32 @@
-import { IonTitle } from '@ionic/react';
+import { IonTitle, IonAlert } from '@ionic/react';
 import { profile } from 'console';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Plugins } from '@capacitor/core';
 import { errorMonitor } from 'events';
+import { getName } from 'ionicons/dist/types/components/icon/utils';
 const { Geolocation, Storage } = Plugins;
 const Profil: React.FC = () => {
+
     //Hooks useState
-    const [name, setName] = useState('Jordan')
+    const [name, setName] = useState()
     const [latitude, setLatitude] = useState()
     const [longitude, setLongitude] = useState()
+    const [showEditUsernameAlert, setShowEditUsernameAlert] = useState(false);
 
     //Similaire à componentDidMount et componentDidUpdate
     useEffect(() => {
         getPosition()
-            .then((res) => { console.log(res) })
-            .catch((error) => { console.log(error) })
+            .then((res) => {console.log(res)})
+            .catch((error) => {console.log(error)})
+        getName()
+            .then((res) => {console.log(res)})
+            .catch((error) => {console.log(error)})
     })
+
     //Récuperation données position
     async function getPosition() {
-        const ret = await Storage.get({ key: 'position' });
-        const value: string = (ret.value != null) ? ret.value : '';
+        const ret  = await Storage.get({ key: 'position' });
+        const value : string = (ret.value != null) ? ret.value : '';
         const position = JSON.parse(value)
         console.log(position)
         setLatitude(position.latitude)
@@ -27,13 +34,63 @@ const Profil: React.FC = () => {
         return position
     }
 
+    interface Iprofil {
+        nameUpdate ?: string,
+    }
 
+    // Mise a jour du profil
+    const saveName = (name: string) => {
+        Storage.set({
+            key : 'profil',
+            value : name
+        })
+        console.log(name)
+    }
 
-    return (
+    //Récuperation données profil
+    async function getName() {
+        const ret  = await Storage.get({ key: 'profil' });
+        const value : string = (ret.value != null) ? ret.value : '';
+        const name : any = value
+        console.log(name)
+        setName(name)
+        return name
+    }
+
+    return(
         <>
-            <IonTitle>Username : {name}</IonTitle>
+            <IonTitle onClick={() => setShowEditUsernameAlert(true)}>Username : {name}</IonTitle>
             <IonTitle>Last latitude : {latitude}</IonTitle>
             <IonTitle>Last longitude : {longitude}</IonTitle>
+
+            <IonAlert
+                isOpen={showEditUsernameAlert}
+                onDidDismiss={() => setShowEditUsernameAlert(false)}
+                header={'Change Username?'}
+                inputs={[
+                    {
+                        name: 'usernameInput',
+                        type: 'text',
+                        id: 'profile-username',
+                        value: name,
+                        placeholder: 'Your username'
+                    }]}
+                buttons={[
+                    {
+                        text: 'Cancel',
+                        role: 'cancel',
+                        handler: () => {
+                            console.log('Confirm Cancel');
+                        }
+                    },
+                    {
+                        text: 'Ok',
+                        handler: (alertData) => saveName(alertData.usernameInput)
+                    }
+                ]}
+
+            />
+
         </>
     )
 }
